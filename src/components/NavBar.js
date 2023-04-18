a2e40ecb907382b491c54bd177ee9f540545b816
Tina Shi <init.defaultBranch>
2023-04-18 17:41:01 +1000

    commit tree fb11972a4194e98b58716b4c6f4487d3307cc1c5

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,17 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{
+      links.map((value, i) => {
+        return (
+          <a key={i}>{value}</a>)
+          
+        // need to get href in
+      })
+    }</nav>;
 }
 
 export default NavBar;
+
+// (
+//   <a href="#home">home</a>, <a href="#about">about</a>,<a href="#projects">projects</a>)
